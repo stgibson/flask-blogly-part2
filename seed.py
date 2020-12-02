0d3e66fc35585acdca3c6655cc95e56057b254b8
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-01 17:13:25 -0800

    Now using zip function to create users. Modified seed.py.

--- seed.py
+++ seed.py
@@ -6,12 +6,14 @@ db.create_all()
 
 User.query.delete()
 
-users = []
-users.append(User(first_name="Alan", last_name="Alda", image_url=
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Alan_Alda_circa_1960s.JPG/800px-Alan_Alda_circa_1960s.JPG"
-))
-users.append(User(first_name="Joel", last_name="Burton"))
-users.append(User(first_name="Jane", last_name="Smith"))
+first_names = ("Alan", "Joel", "Jane")
+last_names = ("Alda", "Burton", "Smith")
+image_urls = (
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Alan_Alda_circa_1960s.JPG/800px-Alan_Alda_circa_1960s.JPG",
+    "",
+    ""
+)
+users = [User(first_name=user[0], last_name=user[1], image_url=user[2]) for user in zip(first_names, last_names, image_urls)]
 
 db.session.add_all(users)
 db.session.commit()
