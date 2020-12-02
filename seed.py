97fdbb8103d7d91789df7bf4223fa7593b02e28b
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-01 17:35:28 -0800

    Adding posts to db in seed.py.

--- seed.py
+++ seed.py
@@ -5,6 +5,7 @@ db.drop_all()
 db.create_all()
 
 User.query.delete()
+Post.query.delete()
 
 first_names = ("Alan", "Joel", "Jane")
 last_names = ("Alda", "Burton", "Smith")
@@ -13,7 +14,17 @@ image_urls = (
     "",
     ""
 )
-users = [User(first_name=user[0], last_name=user[1], image_url=user[2]) for user in zip(first_names, last_names, image_urls)]
+users = [User(first_name=user[0], last_name=user[1], image_url=user[2]) \
+    for user in zip(first_names, last_names, image_urls)]
 
 db.session.add_all(users)
+db.session.commit()
+
+titles = ("MASH", "Quote", "Dev")
+contents = ("I very much so enjoyed starring in it", "Loneliness is everything it's cracked up to be", "I am an expert")
+user_ids = (1, 1, 2)
+posts = [Post(title=post[0], content=post[1], user_id=post[2]) for post in \
+    zip(titles, contents, user_ids)]
+
+db.session.add_all(posts)
 db.session.commit()
